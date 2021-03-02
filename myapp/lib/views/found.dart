d692f9a01f1f590199d06230da5a9d76289a8954
chang <1792286994@qq.com>
2021-03-02 10:02:02 +0800

    底部弹出框

--- myapp/lib/views/found.dart
+++ myapp/lib/views/found.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FoundPage extends StatelessWidget {
@@ -8,6 +9,90 @@ class FoundPage extends StatelessWidget {
         title: Text('FoundPage'),
         elevation: 0.0,
       ),
+      body: UserOptions(),
     );
+  }
+}
+
+class UserOptions extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new UserOptionsState();
+  }
+}
+
+class UserOptionsState extends State<UserOptions> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Builder(
+        builder: (BuildContext context) {
+          return Center(
+            child: RaisedButton(
+                child: Text('Like my Work ?'),
+                onPressed: () {
+                  containerForSheet<String>(
+                    context: context,
+                    child: CupertinoActionSheet(
+                        title: const Text('标题'),
+                        message: const Text('这里是消息'),
+                        actions: <Widget>[
+                          CupertinoActionSheetAction(
+                            child: const Text('🙋 Yes'),
+                            onPressed: () {
+                              Navigator.pop(context, '🙋 Yes');
+                            },
+                          ),
+                          CupertinoActionSheetAction(
+                            child: const Text('🙋 No'),
+                            onPressed: () {
+                              Navigator.pop(context, '🙋 No');
+                            },
+                          ),
+                          CupertinoActionSheetAction(
+                            child: const Text("🙋 Can't say"),
+                            onPressed: () {
+                              Navigator.pop(context, "🙋 Can't say");
+                            },
+                          ),
+                          CupertinoActionSheetAction(
+                            child: const Text("🙋 Decide in next post"),
+                            onPressed: () {
+                              Navigator.pop(context, "🙋 Decide in next post");
+                            },
+                          ),
+                        ],
+                        cancelButton: CupertinoActionSheetAction(
+                          child: new Text(
+                            '取消',
+                            style: TextStyle(
+                              fontSize: 13.0,
+                              fontFamily: 'PingFangRegular',
+                              color: const Color(0xFF666666),
+                            ),
+                          ),
+                          isDefaultAction: true,
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        )),
+                  );
+                }),
+          );
+        },
+      ),
+    );
+  }
+
+  void containerForSheet<T>({BuildContext context, Widget child}) {
+    showCupertinoModalPopup<T>(
+      context: context,
+      builder: (BuildContext context) => child,
+    ).then<void>((T value) {
+      Scaffold.of(context).showSnackBar(new SnackBar(
+        content: new Text('You clicked $value'),
+        duration: Duration(milliseconds: 800),
+      ));
+    });
   }
 }
---
+++ myapp/lib/widget/build_text_fied.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class LoginTextField extends TextField {
+  LoginTextField({
+    Key key,
+    String text,
+
+    String placeholder,
+
+    /// prefix icon
+    bool prefixIconSelected,
+    String prefixIconNormalImageName,
+    String prefixIconSelectedImageName,
+
+    bool autofocus = false,
+    bool obscureText = false,
+    TextInputType keyboardType,
+    TextEditingController controller,
+    bool showClear = false,
+    TextInputAction textInputAction,
+    FocusNode focusNode,
+    ValueChanged<String> onSubmitted,
+  }) : super(
+      key: key,
+      autofocus: autofocus,
+      obscureText: obscureText,
+      style: TextStyle(color: Colors.black, fontSize: 17.0),
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(0.0),
+        //labelText: "用户名",
+        hintText: placeholder,
+        //prefixIcon: Icon(Icons.person),
+        prefixIcon: new Image.asset(
+          !prefixIconSelected ? prefixIconNormalImageName :prefixIconSelectedImageName,
+          width: 14.0,
+          height: 15.0,
+        ),
+        // suffixIcon: !showClear ? null : clearButtonWithOnPressed(controller.clear),
+        enabledBorder: loginTextFieldDecorationBorder(),
+        focusedBorder: loginTextFieldDecorationBorder(),
+      ),
+      keyboardType: keyboardType,
+      controller: controller,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      onSubmitted: onSubmitted
+  );
+}
+
+/// selected Image
+class SelectedImage extends Image {
+  SelectedImage({
+    Key key,
+    bool selected,
+    String normalImageName,
+    String selectedImageName,
+  }) :super (
+      key: key,
+      image: AssetImage(!selected ? normalImageName :selectedImageName)
+  );
+}
+
+
+/// 文本框border
+InputBorder loginTextFieldDecorationBorder() {
+  return new OutlineInputBorder(
+      borderSide: new BorderSide(color: Color(0xffd2d2d2), width: 0.6),
+      borderRadius: new BorderRadius.circular(6.0)
+  );
+}
