89a8087584b5c11f30a16ed3a18f07981908e8c7
chang <1792286994@qq.com>
2021-03-04 15:28:05 +0800

    图片上传

--- myapp/lib/views/found.dart
+++ myapp/lib/views/found.dart
@@ -194,6 +194,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<File> _imageFile = [];
+  List<File> _videoFile = [];
   File _image;
   File _cameraImage;
   File _video;
@@ -203,17 +204,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   VideoPlayerController _videoPlayerController;
   VideoPlayerController _cameraVideoPlayerController;
+  Future<void> _initializeVideoPlayerFuture;
 
-  /*图片控件*/
-  Widget _ImageView(imgPath) {
-    if (imgPath == null) {
-      return Center();
-    } else {
-      Image.file(
-        imgPath,
-      );
-    }
-  }
 
   // This funcion will helps you to pick and Image from Gallery
   _pickImageFromGallery() async {
@@ -244,7 +236,7 @@ class _MyHomePageState extends State<MyHomePage> {
     PickedFile pickedFile = await picker.getVideo(source: ImageSource.gallery);
 
     _video = File(pickedFile.path);
-
+    _videoFile.add(_video);
     _videoPlayerController = VideoPlayerController.file(_video)
       ..initialize().then((_) {
         setState(() {});
@@ -257,13 +249,16 @@ class _MyHomePageState extends State<MyHomePage> {
     PickedFile pickedFile = await picker.getVideo(source: ImageSource.camera);
 
     _cameraVideo = File(pickedFile.path);
-
+    _videoFile.add(_cameraVideo);
     _cameraVideoPlayerController = VideoPlayerController.file(_cameraVideo)
       ..initialize().then((_) {
         setState(() {});
         _cameraVideoPlayerController.play();
       });
   }
+
+  //上传图片用
+  ScrollController _imgController = new ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -301,7 +296,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     itemBuilder: (BuildContext context, index) {
                       return Wrap(
                         // spacing: 8.0, // 主轴(水平)方向间距
-                        // runSpacing: 4.0, // 纵轴（垂直）方向间距
+                        runSpacing: 4.0, // 纵轴（垂直）方向间距
                         alignment: WrapAlignment.center, //沿主轴方向居中
                         children: <Widget>[
                           Image.file(
@@ -311,6 +306,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             fit: BoxFit.fill,
                           ),
                         ],
+
                       );
                     }),
                 _imageFile.length >= 6
@@ -346,8 +342,100 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ]),
         ),
+
+        Card(
+          child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+            Container(
+              height: 40,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(left: 16),
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(width: 1, color: Color(0xffe5e5e5)))
+//              border: Border.all(color: Colors.grey,width: 0.5),
+//              borderRadius: BorderRadius.circular(5),
+              ),
+              child: Text('视频（六张）', style: TextStyle(color: Colors.green)),
+            ),
+            Wrap(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+
+                _videoFile.length == 0
+                    ? Container()
+                    : GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        childAspectRatio: 1,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10),
+                    padding: EdgeInsets.all(10),
+                    shrinkWrap: true,
+                    itemCount: _videoFile.length>6?6:_videoFile.length,
+                    // itemCount: _imageFile.length,
+                    itemBuilder: (BuildContext context, index) {
+                      return Wrap(
+                        // spacing: 8.0, // 主轴(水平)方向间距
+                        runSpacing: 4.0, // 纵轴（垂直）方向间距
+                        alignment: WrapAlignment.center, //沿主轴方向居中
+                        children: <Widget>[
+                          Image.file(
+                            _videoFile[index],
+                            height: 80,
+                            width: 80,
+                            fit: BoxFit.fill,
+                          ),
+                        ],
+
+                      );
+                    }),
+                _videoFile.length >= 6
+                    ? Container()
+                    :Container(
+                  margin: EdgeInsets.all(6),
+                  //容器外补白
+                  constraints: BoxConstraints.tightFor(width: 80.0, height: 80.0),
+                  //卡片大小
+                  decoration: BoxDecoration(
+                    //背景装饰
+                    gradient: RadialGradient(
+                      //背景径向渐变
+                        colors: [Color(0xfff6f6f6),Color(0xfff6f6f6)],
+                        center: Alignment.topLeft,
+                        radius: .6),
+                  ),
+                  alignment: Alignment.center,
+                  //卡片内文字居中
+                  child: FlatButton(
+                    height: 80,
+                    minWidth: 80,
+                    splashColor: Colors.grey,
+                    child: Icon(
+                      Icons.add,
+                      color: Color(0xffcacaca),
+                      size: 24.0,
+                    ),
+                    onPressed: _videoView,
+                  ),
+                ),
+              ],
+            ),
+          ]),
+        ),
       ]),
     );
+  }
+
+  void containerForSheet<T>({BuildContext context, Widget child}) {
+    showCupertinoModalPopup<T>(
+      context: context,
+      builder: (BuildContext context) => child,
+    ).then<void>((T value) {
+      Scaffold.of(context).showSnackBar(new SnackBar(
+        content: new Text('You clicked $value'),
+        duration: Duration(milliseconds: 800),
+      ));
+    });
   }
   _imgView() {
     containerForSheet<String>(
@@ -370,6 +458,31 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pop(context,);
               },
             ),
+          ],
+          cancelButton: CupertinoActionSheetAction(
+            child: new Text(
+              '取消',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontFamily: 'PingFangRegular',
+                color: const Color(0xFF666666),
+              ),
+            ),
+            isDefaultAction: true,
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          )),
+    );
+  }
+
+  _videoView() {
+    containerForSheet<String>(
+      context: context,
+      child: CupertinoActionSheet(
+        // title: const Text('标题'),
+        // message: const Text('这里是消息'),
+          actions: <Widget>[
             CupertinoActionSheetAction(
               child: const Text("从文件选视频"),
               onPressed: () {
@@ -389,7 +502,7 @@ class _MyHomePageState extends State<MyHomePage> {
             child: new Text(
               '取消',
               style: TextStyle(
-                fontSize: 13.0,
+                fontSize: 16.0,
                 fontFamily: 'PingFangRegular',
                 color: const Color(0xFF666666),
               ),
@@ -401,15 +514,6 @@ class _MyHomePageState extends State<MyHomePage> {
           )),
     );
   }
-  void containerForSheet<T>({BuildContext context, Widget child}) {
-    showCupertinoModalPopup<T>(
-      context: context,
-      builder: (BuildContext context) => child,
-    ).then<void>((T value) {
-      Scaffold.of(context).showSnackBar(new SnackBar(
-        content: new Text('You clicked $value'),
-        duration: Duration(milliseconds: 800),
-      ));
-    });
-  }
+
 }
+
